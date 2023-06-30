f66134f59789e4894e6227a247cdfe41411c68e9
s24039 <128316248+s24039@users.noreply.github.com>
2023-06-30 09:43:59 +0200

    add initial migration

---
+++ Migrations/20230630074317_initial.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    IdTeam = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Deadline = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.IdTeam);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaskTypes",
+                columns: table => new
+                {
+                    IdTaskType = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskTypes", x => x.IdTaskType);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TeamMembers",
+                columns: table => new
+                {
+                    IdTeamMember = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeamMembers", x => x.IdTeamMember);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    IdTask = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Deadline = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IdTeam = table.Column<int>(type: "int", nullable: false),
+                    IdTaskType = table.Column<int>(type: "int", nullable: false),
+                    IdAssignedTo = table.Column<int>(type: "int", nullable: false),
+                    IdCreator = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.IdTask);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Project_IdTeam",
+                        column: x => x.IdTeam,
+                        principalTable: "Project",
+                        principalColumn: "IdTeam",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tasks_TaskTypes_IdTaskType",
+                        column: x => x.IdTaskType,
+                        principalTable: "TaskTypes",
+                        principalColumn: "IdTaskType",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tasks_TeamMembers_IdAssignedTo",
+                        column: x => x.IdAssignedTo,
+                        principalTable: "TeamMembers",
+                        principalColumn: "IdTeamMember",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tasks_TeamMembers_IdCreator",
+                        column: x => x.IdCreator,
+                        principalTable: "TeamMembers",
+                        principalColumn: "IdTeamMember",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_IdAssignedTo",
+                table: "Tasks",
+                column: "IdAssignedTo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_IdCreator",
+                table: "Tasks",
+                column: "IdCreator");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_IdTaskType",
+                table: "Tasks",
+                column: "IdTaskType");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_IdTeam",
+                table: "Tasks",
+                column: "IdTeam");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Project");
+
+            migrationBuilder.DropTable(
+                name: "TaskTypes");
+
+            migrationBuilder.DropTable(
+                name: "TeamMembers");
+        }
+    }
+}
